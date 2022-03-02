d3cc6fb02e6e3012a8ceb8aafec1dd63d538b92e
Richard Corkery <RichardCorkery@hotmail.com>
2022-03-02 15:54:07 -0500

    Add API controller)

---
+++ PasLookupData.Api/Controllers/AboutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PasLookupData.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        // GET: api/<AboutController>
+        [HttpGet]
+        public string Get()
+        {
+            return "About Controller";
+        }
+    }
+}
